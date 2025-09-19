50b23138d70ff6373ad0ec3faa5a0fefafa34d46
ponolite <147739831+ponolite@users.noreply.github.com>
2025-09-18 21:31:18 -0400

    update codes

--- streamlit_sample.py
+++ streamlit_sample.py
@@ -1,13 +1,18 @@
 import streamlit as st
 
+if globals().get('page_title') is None:
+    page_title = "👋 Hello from INFO-5940!"
+
 st.set_page_config(page_title="Hello Codespaces", layout="centered")
 
-st.title("👋 Hello from Codespaces!")
+st.title(page_title)
+
 st.write("If you can see this page, Streamlit is running correctly inside your Codespace.")
 
 name = st.text_input("What is your name?")
 if name:
     st.success(f"Nice to meet you, {name}!")
+    page_title = "We're one testin"
 
 st.markdown("---")
 st.caption("This is a test app for INFO 5940 Fall 2025.")
